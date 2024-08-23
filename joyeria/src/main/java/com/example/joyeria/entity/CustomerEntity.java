3430d5e6b5a80f137cd95b67b08718af037a069a
EvryDash05 <elpandaeverjoelccencho@gmail.com>
2024-08-22 21:29:07 -0500

    First commit

---
+++ joyeria/src/main/java/com/example/joyeria/entity/CustomerEntity.java
@@ -0,0 +1,28 @@
+package com.example.joyeria.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_customers")
+public class CustomerEntity {
+
+    @Id
+    @Column(name = "customer_id", nullable = false, length = 7)
+    private String id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "last_name", nullable = false, length = 50)
+    private String lastName;
+
+}
