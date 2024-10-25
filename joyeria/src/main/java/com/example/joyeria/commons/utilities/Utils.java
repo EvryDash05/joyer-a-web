351c5c627091b3e3786b41b0eebaab10d654665f
EvryDash05 <elpandaeverjoelccencho@gmail.com>
2024-10-25 17:01:10 -0500

    Se corrige el nombre dlel método por generateRandomId

--- joyeria/src/main/java/com/example/joyeria/commons/utilities/Utils.java
+++ joyeria/src/main/java/com/example/joyeria/commons/utilities/Utils.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Utils {
 
-    public static String generateId(String prefix){
+    public static String generateRandomId(String prefix){
         Random rand = new Random();
         int randomNumber = rand.nextInt(100, 999);
         return prefix + randomNumber;
