3ed3803a0daeffa6006c7c86a59820cd3aba4417
EvryDash05 <elpandaeverjoelccencho@gmail.com>
2024-10-29 01:04:21 -0500

    Se añade el script para poder iniciar sesión

---
+++ joyeria/src/main/resources/static/js/loginForm.js
@@ -0,0 +1,33 @@
+const ENDPOINT_REGISTER_USER = 'http://localhost:8080/v1/api/login'
+
+document.querySelector('#login-form').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(e.target))
+
+    try {
+        const response = await fetch(ENDPOINT_REGISTER_USER, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+
+        if (response.ok) {
+            const customerData = await response.json();
+
+            localStorage.setItem('customerData', JSON.stringify(customerData));
+
+            alert("Login successfully");
+
+            window.location.href = "../../templates/index.html";
+        } else {
+            alert("Error to login")
+        }
+
+    } catch (e) {
+        alert("Error to login")
+        console.error(e)
+    }
+
+})
