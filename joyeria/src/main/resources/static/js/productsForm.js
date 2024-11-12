804e5a1940c3526332df2ad15b49f1de56a8bca7
EvryDash05 <elpandaeverjoelccencho@gmail.com>
2024-11-12 11:50:25 -0500

    Se añade el campo para aceptar archivos y se crea el script para subir el registro

--- joyeria/src/main/resources/static/js/productsForm.js
+++ joyeria/src/main/resources/static/js/productsForm.js
@@ -1,24 +1,35 @@
 
-const ENDPOINT_REGISTER_PRODUCT = 'http://localhost:8080/v1/api/createProduct'
+const ENDPOINT_REGISTER_PRODUCT = 'http://localhost:8080/v1/api/createProduct';
 
 document.querySelector('#product-form').addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    const data = Object.fromEntries(new FormData(e.target));
-    console.log(data);
+    const formData = new FormData();
 
-    const response = await fetch(ENDPOINT_REGISTER_PRODUCT, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: data
-    }).catch(e => {
-        console.log(e)
-    })
+    const data = {
+        name: e.target.name.value,
+        description: e.target.description.value,
+        price: e.target.price.value,
+        quantity: e.target.quantity.value
+    };
+    formData.append("product", JSON.stringify(data));
 
-    if(response.ok){
-        console.log("Producto creado")
+    const imgFile = e.target.img.files[0];
+    formData.append("img", imgFile);
+
+    try {
+        const response = await fetch(ENDPOINT_REGISTER_PRODUCT, {
+            method: 'POST',
+            body: formData
+        });
+
+        if (response.ok) {
+            document.getElementById('product-form').reset()
+            console.log("Producto creado");
+        } else {
+            console.log("Error al crear el producto");
+        }
+    } catch (error) {
+        console.error("Error en la solicitud:", error);
     }
-
-})
+});
