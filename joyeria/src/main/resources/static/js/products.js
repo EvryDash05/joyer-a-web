be4ff27302ccec9ac47389c69e82ea85ee844d0d
EvryDash05 <elpandaeverjoelccencho@gmail.com>
2024-11-12 11:53:47 -0500

    Se añade el script para poder ver la lista de productos

--- joyeria/src/main/resources/static/js/products.js
+++ joyeria/src/main/resources/static/js/products.js
@@ -1,30 +1,32 @@
 
 function addProductCard(prd) {
     const productsContainer = document.getElementById('product-container')
-
-    console.log(prd)
         productsContainer.innerHTML += `
             <div class="col">
                 <div class="card h-100 product-card">
                     <span class="position-absolute top-0 start-0 badge m-2" style="background-color: #12171b; color: #fff;">HOT</span>
-                        <img src="/img/products/${prd}" class="card-img-top product-image original-img" alt="Chicago Hoops">
-                        <img src="/img/alt.png" class="card-img-top product-img-hover" alt="Chicago Hoops Hover">
+                        <img src="data:image/*;base64,${prd.img}" class="card-img-top product-image original-img" alt="Chicago Hoops">
+                        <img src="/img/products/alt.png" class="card-img-top product-img-hover" alt="Chicago Hoops Hover">
                         <div class="card-body" style="background-color: #F6F0EE;">
-                            <h5 class="card-title">HOPE ESMERALDA</h5>
-                            <p class="card-text">40.00$ – 60.00$</p>
+                            <h5 class="card-title">${prd.name}</h5>
+                            <p id="product-id" class="card-text">${prd.productId}</p>
+                            <p class="card-text">S/.${prd.price}</p>
                         </div>
-                        <button class="btn btn-link position-absolute top-0 end-0 m-2">
-                            <i class="bi bi-heart"></i>
-                        </button>
                     </div>
+                    <button class="btn btn-primary" onclick="showProductDetails()">Ver producto</button>
                 </div>
             </div>
         `
 
 }
 
+function showProductDetails() {
+    const productId = document.getElementById('product-id').innerText
+    window.location.href = `/front/productDetail/${productId}`
+}
+
 async function showProducts() {
-    const response = await fetch('http://localhost:8080/v1/api/images')
+    const response = await fetch('http://localhost:8080/v1/api/getProducts');
     const products = await response.json()
     products.map(prd => addProductCard(prd));
 }
