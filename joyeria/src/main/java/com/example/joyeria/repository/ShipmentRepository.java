9c85ad78811012af1d6019617b056b6c69a91965
EvryDash05 <elpandaeverjoelccencho@gmail.com>
2024-11-13 18:53:18 -0500

    Se agrega el método para poder encontrar envíos mediante el id del cliente

--- joyeria/src/main/java/com/example/joyeria/repository/ShipmentRepository.java
+++ joyeria/src/main/java/com/example/joyeria/repository/ShipmentRepository.java
@@ -2,8 +2,14 @@ package com.example.joyeria.repository;
 
 import com.example.joyeria.entities.ShipmentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ShipmentRepository extends JpaRepository<ShipmentEntity, String> {
+    @Query("SELECT s FROM ShipmentEntity s WHERE s.customer.customerId = :customerId")
+    List<ShipmentEntity> findByCustomerId(@Param("customerId") String customerId);
 }
