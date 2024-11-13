c971d1201eea228d4a53beeb194752b443300237
EvryDash05 <elpandaeverjoelccencho@gmail.com>
2024-11-13 02:12:22 -0500

    Se corrige la función addProductCard(prd)

--- joyeria/src/main/resources/static/js/products.js
+++ joyeria/src/main/resources/static/js/products.js
@@ -1,27 +1,25 @@
 
 function addProductCard(prd) {
-    const productsContainer = document.getElementById('product-container')
-        productsContainer.innerHTML += `
-            <div class="col">
-                <div class="card h-100 product-card">
-                    <span class="position-absolute top-0 start-0 badge m-2" style="background-color: #12171b; color: #fff;">HOT</span>
-                        <img src="data:image/*;base64,${prd.img}" class="card-img-top product-image original-img" alt="Chicago Hoops">
-                        <img src="/img/products/alt.png" class="card-img-top product-img-hover" alt="Chicago Hoops Hover">
-                        <div class="card-body" style="background-color: #F6F0EE;">
-                            <h5 class="card-title">${prd.name}</h5>
-                            <p id="product-id" class="card-text">${prd.productId}</p>
-                            <p class="card-text">S/.${prd.price}</p>
-                        </div>
+    const productsContainer = document.getElementById('product-container');
+    productsContainer.innerHTML += `
+        <div class="col">
+            <div class="card h-100 product-card">
+                <span class="position-absolute top-0 start-0 badge m-2" style="background-color: #12171b; color: #fff;">HOT</span>
+                    <img src="data:image/*;base64,${prd.img}" class="card-img-top product-image original-img" alt="Chicago Hoops">
+                    <img src="/img/products/alt.png" class="card-img-top product-img-hover" alt="Chicago Hoops Hover">
+                    <div class="card-body" style="background-color: #F6F0EE;">
+                        <h5 class="card-title">${prd.name}</h5>
+                        <p class="card-text">S/.${prd.price}</p>
+                        <p id="product-id" class="card-text">${prd.productId}</p>
                     </div>
-                    <button class="btn btn-primary" onclick="showProductDetails('${prd.productId}')">Ver producto</button>
-               </div>
+                </div>
+                <button class="btn btn-primary" onclick="showProductDetails('${prd.productId}')">Ver producto</button>
             </div>
-        `
-
+        </div>
+    `;
 }
 
-function showProductDetails() {
-    const productId = document.getElementById('product-id').innerText
+function showProductDetails(productId) {
     window.location.href = `/front/productDetail/${productId}`
 }
 
